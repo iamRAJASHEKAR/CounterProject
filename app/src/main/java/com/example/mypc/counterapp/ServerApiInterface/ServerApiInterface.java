8087871da3606677ffd4be9cd4098b29e961827c
MadhuraReddy <madhura2419@gmail.com>
2018-06-21 17:55:57 +0530

    changes

---
+++ app/src/main/java/com/example/mypc/counterapp/ServerApiInterface/ServerApiInterface.java
@@ -0,0 +1,7 @@
+package com.example.mypc.counterapp.ServerApiInterface;
+
+public interface ServerApiInterface
+{
+    public static String Base_Url = "://107.175.83.105/CounterApp/webapi/counter";
+
+}
